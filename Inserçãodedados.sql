e50b481cb8057ba3503d76f4273726925ab4d09c
Vincius-Drums <anaogeraldino@gmail.com>
2024-04-05 21:10:38 -0300

    [M2S06] Ex 02 - Inserção de dados SQL

---
+++ Inserçãodedados.sql
@@ -0,0 +1,16 @@
+CREATE TABLE herois (
+    identificador SERIAL PRIMARY KEY,
+    nome VARCHAR NOT NULL,
+    superpoder VARCHAR,
+    idade INTEGER,
+    data_cadastro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Inserir os dados na tabela "herois"
+INSERT INTO herois (nome, superpoder, idade, data_cadastro) 
+VALUES 
+    ('Spiderman', 'teia', 18, CURRENT_TIMESTAMP),
+    ('Iron Man', 'armadura', 40, CURRENT_TIMESTAMP),
+    ('Batman', 'bat-coisas', 39, CURRENT_TIMESTAMP),
+    ('Superman', 'super-força', 26, CURRENT_TIMESTAMP);
+
